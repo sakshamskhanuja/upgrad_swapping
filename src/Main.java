57c3054bad63700e387e6244cae9421b0f61b9c1
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-08 15:29:42 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 2;
+
+        // Code exchanges values of a and b.
+        int c = a;
+        a = b;
+        b = c;
+
+        // Code prints values of a and b.
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+}
